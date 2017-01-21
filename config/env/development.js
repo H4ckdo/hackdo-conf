8db7353d61501eb3b23fd9c789f436ad0763f9f6
maxtermax <esnene02@gmail.com>
2017-01-21 12:21:29 -0500

    test env

--- config/env/development.js
+++ config/env/development.js
@@ -10,9 +10,32 @@
  *
  */
 process.env.DEV = true;
+let fixtures;
+if(process.env.STAGING || process.env.DEV) {
+  fixtures = {
+      order: ['User', 'Event'],
+      User: [
+        {
+          name: 'esneyder',
+          email: 'esnene02@gmail.com',
+          password: '123456',
+          rol: 'superadmin'
+        }
+      ],
+      Event: [
+        {
+          name: "Hackdo",
+          startAt: "2017/01/03",
+          endAt: "2017/03/03",
+          eventType: "presential",
+          location: "Quibdo Calle 24 18-128"
+        }
+      ]
+    };
+};
 
 module.exports = {
-
+  fixtures,
   /***************************************************************************
    * Set the default database connection for models in the development       *
    * environment (see config/connections.js and config/models.js )           *
