5f43e5737413eb1bdcf088d1ba2456a5904f67dc
maxtermax <esnene02@gmail.com>
2017-01-20 23:04:23 -0500

    try newman

--- newman_test.js
+++ newman_test.js
@@ -2,7 +2,7 @@ const exec = require('child_process').exec;
 module.exports = function() {
   return (new Promise(function(resolve, reject) {
     console.log("TRY API TEST");
-      exec(`newman -h && ls`, (err, stdout, stderr) => {
+      exec(`newman run hackdo_website.postman_collection.json --bail`, (err, stdout, stderr) => {
         if (err) return reject(err);
         return resolve(stdout);
       });
