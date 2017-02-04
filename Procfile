165d1387f3c7af4e84a021efd22c14312f380b9e
maxtermax <esnene02@gmail.com>
2017-02-04 17:13:46 -0500

    cron python deleted

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: node app.js
-clock: python clock.py
--- clock.py
+++
@@ -1,9 +0,0 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-
-sched = BlockingScheduler()
-
-@sched.scheduled_job('interval', minutes=1)
-def timed_job():
-    print('This job is run every three minutes.')
-
-sched.start()
