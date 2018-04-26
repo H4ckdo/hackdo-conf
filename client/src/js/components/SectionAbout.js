1d490e743e317986fc2368ec6a97ff01a5463be5
Fredy Mena Andrade <fry.andrade@gmail.com>
2018-04-25 23:59:05 -0500

    sectionAbout modified.

--- client/src/js/components/SectionAbout.js
+++ client/src/js/components/SectionAbout.js
@@ -18,9 +18,9 @@ class SectionAbout extends React.Component {
               </h1>
 
               <p className="section-about__article__copy">
-                Hackdó Conf, es el evento anual llevado a cabo por la Hackdó Fundation que reúne a entusiasta emprendedores y desarrolladores durante dos días en la ciudad de Quibdó.
+                Hackdó Conf, es el evento anual llevado a cabo por la Hackdó Foundation y Partners, que reúne a entusiasta emprendedores y desarrolladores durante dos días en la ciudad de Quibdó.
                 <br/>
-                Más de 2 años impactando el ecosistema de báse tecnolgógica del Chocó, llegando a más de <strong>200 personas con 22 charlas impartidas, 17 talleres sobre 4 lenguajes de programación.</strong>
+                Más de 2 años impactando el ecosistema de base tecnolgógica del Chocó, llegando a más de <strong>200 personas con 22 charlas impartidas, 17 talleres sobre 4 lenguajes de programación además de charlas sobre el internet de las cosas y programación de hardware.</strong>
               </p>
           </article>
 
