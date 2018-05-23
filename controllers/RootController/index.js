daf0fb106b4cc1ed815bab6b21de561104f6a758
maxtermax <esnene02@gmail.com>
2018-05-23 10:23:42 -0500

    turn off cache

--- controllers/RootController/index.js
+++ controllers/RootController/index.js
@@ -18,6 +18,9 @@ webpush.setVapidDetails(
  */
 class RootController {
   main(req, res) {
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
     res.render('index', { environment, publicServerKey })
   }
 
